7117805b837e486b3b8ea7ede19848e9047bba8a
Nicovz21 <61903751+Nicovz21@users.noreply.github.com>
2021-10-08 04:27:07 -0400

    debug instead of info for logger

--- logger.js
+++ logger.js
@@ -6,7 +6,7 @@ winston.format.printf(({ level, message, timestamp}) => {
 });
 
 const logger = winston.createLogger({
-    level: 'info',
+    level: 'debug',
     format: winston.format.combine(
         winston.format.colorize(),
         winston.format.timestamp({ format: 'YYYY-MM-DD-HH:mm:ss'}),
